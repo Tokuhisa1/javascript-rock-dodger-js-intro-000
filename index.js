a23afcde2e0ef5080cdac0246e69f8fc25099942
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 18:35:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -80,12 +80,12 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-
+    if (checkCollision(rock)) { endGame(); }
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
      */
-
+    if (rock.style.top < 400) { moveRock(); }
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
